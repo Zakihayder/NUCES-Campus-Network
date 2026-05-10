7edc10cc8404c0807051c57b93792921950175f0
Zakihayder <zakihaider7860@gmail.com>
2026-05-10 17:58:57 +0500

    Add complete NUCES Campus Network simulation files

--- out/clang-release/src/StaticRouter.o.d
+++
@@ -1,5 +0,0 @@
-out/clang-release//src/StaticRouter.o: src/StaticRouter.cc \
-  src/StaticRouter.h \
-  D:/Omnetpp/omnetpp-6.3.0-windows-x86_64/omnetpp-6.3.0/include/omnetpp.h
-src/StaticRouter.h:
-D:/Omnetpp/omnetpp-6.3.0-windows-x86_64/omnetpp-6.3.0/include/omnetpp.h:
